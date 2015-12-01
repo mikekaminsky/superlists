c9a6725cd03d2fdcefeef57adea4df7de6678f52
Michael Kaminsky <michaelkaminsky@Michaels-Air.home>
2015-11-30 21:17:17 -0500

    Adding scope to functional tests to handle multiple users.

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -37,9 +37,12 @@ class NewVisitorTest(LiveServerTestCase):
         # User types 'Buy peacock feathers" into a  text box
         inputbox.send_keys('Buy peacock feathers')
 
-        # When they hit enter, the page updates, and now the page lists
-        # 1: Buy peacock feathers" as an item in a to-do list
+        # When they hit enter, they are taken to a new URL,
+        # and now the page lists 1: Buy peacock feathers" as an item 
+        # in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        user_list_url = self.browser.current_url
+        self.assertRegexpMatches(user_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. They 
@@ -53,6 +56,37 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Now a new user, user2, comes along to the site
+
+        ## We use a new browsesr session to make sure that no information
+        ## of the first user's is coming through from cookies etc.
+        self.browser.quit()
+        self.browser.webdriver.Firefox()
+
+        # The second user visits the home page, and there is no sign of user 1's lisquit()t
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # The second user starts a new list by entering a new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        user2_list_url = self.browser.current_url
+        self.assertRegex(user2_list_url, '/lists/.+')
+        self.assertNotEqual(user_list_url, user2_list_url)
+
+        # Double check there's no trace of user 1's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+
+
+
 
         # User wonders wither the site will remember her list. Then they see that the site has generated 
         # a unique URL for them -- there is some explanatory text to that effect.
